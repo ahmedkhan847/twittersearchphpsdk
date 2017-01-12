4c948e3f2cf7cfa256add8328ee9b37d38b01166
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-12 15:10:36 +0500

    working with Twitter API

--- Twitter/TwitterSearch.php
+++ Twitter/TwitterSearch.php
@@ -13,15 +13,42 @@ class TwitterSearch
     const API_URL = "https://api.twitter.com/1.1";
 
     protected $client;
+    protected $token = null;
+    protected $tokensecret = null;
     public function __construct()
     {
         $this->client = new \GuzzleHttp\Client();
+    }
+
+    public function setTokken($token,$secret)
+    {
+        $this->token = $token;
+        $this->tokensecret = $secret;
+    }
+    public function prepareAccessToken()
+    {
+        try{
+            $url = self::API_URL ."/oauth2/token";
+            $value = ['grant_type' => "client_credentials"
+            ];
+            $header = array('Authorization'=>'Basic ' .base64_encode($this->token.":".$this->tokensecret),
+            "Content-Type"=>"application/x-www-form-urlencoded;charset=UTF-8");
+            $response = $this->client->post($url, ['query' => $value,'headers' => $header]);
+            $result = json_decode($response->getBody()->getContents());
+            return $result;
+            $this->accessToken = $result->access_token;
+        }
+        catch (RequestException $e) {
+            $response = $this->statusCodeHandling($e);
+            return $response;
+        }
     }
 
     public function search($value)
     {
         try{
             $url = self::API_URL . "/search/tweets.json";
+            $header = array('Authorization'=>'Bearer ' . $this->accessToken);
             $post = ["q" => $value];
             $response = $this->client->get($url, array('query' => $post,));
             return json_decode($response->getBody()->getContents());
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "vendor/autoload.php";
 use Twitter\TwitterSearch;
 
 $search = new TwitterSearch();
-
+$search->setTokken("afmowdkHQTZVZWMxx0dh5Bz4r","fuJ4HNNTkI27qZEFdv3g4MEMXdVoZHUIVXCwmDbqXBRWMrEQAU");
 echo "<pre>";
-print_r($search->search("sean mcdermott"));
+print_r($search->prepareAccessToken());
 echo "</pre>";
