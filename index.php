df478f203b78435524d37db5fc98726f4f612457
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-12 15:45:17 +0500

    changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "vendor/autoload.php";
 use Twitter\TwitterSearch;
 
 $search = new TwitterSearch();
-$search->setTokken("afmowdkHQTZVZWMxx0dh5Bz4r","fuJ4HNNTkI27qZEFdv3g4MEMXdVoZHUIVXCwmDbqXBRWMrEQAU");
+$search->setTokken("70902893-GPVf9bzJ1ecYEay6cHnQTaQQb95GcmwbIOO02fMOi","8qo1PM1kodTsSZCx5ES6t2dXqPSqta13z1WpDsbibk3dl");
 echo "<pre>";
 print_r($search->search("ahmed khan"));
 echo "</pre>";
