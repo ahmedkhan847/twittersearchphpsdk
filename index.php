27a32c783b117a23eb900289902f88dd0f3fa429
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-12 18:03:46 +0500

    changes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Twitter\Search\Search;
 
 $search = new Search();
 $search->setToken("H9Sq6D2bywCSnaJkDbYgNmIeC","PQ3MSBdAjLEQUfRo2Mg0z1BRoZDuREbbWAezahHjM7GlpelhxY");
-$value = ["q" => "ahmed khan"];
+$value = ["q" => "twitter"];
 echo "<pre>";
 print_r($search->search($value));
 echo "</pre>";
