1b8a2f7f3dd4e5c45d6aeedb66c6829f56b451e0
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-19 15:39:55 +0500

    changes

---
+++ custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    init();
+});
+
+function init() {
+    $.ajax({
+        // Post select to url.
+        type: 'get',
+        url: 'http://phpstack-13267-59727-161633.cloudwaysapps.com/twitter/twittersearchphpsdk/index.php',
+        headers: { 'origin': 'cloudways' },
+	dataType: 'json', // expected returned data format.
+        statusCode: {
+                200: function (response) {
+                    console.log(response);
+                },
+                422: function (response) {
+                   alert('Error Getting Data');
+                },
+                400: function (response) {
+                    alert('URL Not Found', 4000);
+                }
+            }
+    });
+
+}
