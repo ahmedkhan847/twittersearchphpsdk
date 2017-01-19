de465944adb7b354f56d672ef3d86c749fd181b4
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-19 15:35:33 +0500

    changes

--- Twitter/Lists/Lists.php
+++ Twitter/Lists/Lists.php
@@ -6,7 +6,19 @@ namespace Twitter\Lists;
  */
 class Lists extends \Twitter\Base
 {
-    
+    public function getUserList($value)
+    {
+        try{
+            
+            $url = "/lists/list.json";
+            $response = $this->callTwitter("value",$url,$value);
+            return $response;
+        } catch (RequestException $e) {
+            $response = $this->StatusCodeHandling($e);
+            return $response;
+        }
+    }
+
     public function getSpecifiedList($value)
     {
         try{           
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "vendor/autoload.php";
 use Twitter\Search\Search;
 
 $search = new Search();
-$search->setToken("YOUR-CONSUMER-KEY","YOUR-CONSUMER-KEY");
+$search->setToken("H9Sq6D2bywCSnaJkDbYgNmIeC","	PQ3MSBdAjLEQUfRo2Mg0z1BRoZDuREbbWAezahHjM7GlpelhxY");
 $value = ["q" => "twitter"];
 echo "<pre>";
 print_r($search->search($value));
