5fd561fb98907a0ee632fedc728fdeb8ae292c56
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-19 16:35:04 +0500

    changes

--- custom.js
+++ custom.js
@@ -6,8 +6,7 @@ function init() {
     $.ajax({
         // Post select to url.
         type: 'get',
-        url: 'http://phpstack-13267-59727-161633.cloudwaysapps.com/twitter/twittersearchphpsdk/index.php',
-        headers: { 'origin': 'cloudways' },
+        url: 'http://phpstack-13267-59727-161633.cloudwaysapps.com/twitter/twittersearchphpsdk/index2.php',
 	dataType: 'json', // expected returned data format.
         statusCode: {
                 200: function (response) {
