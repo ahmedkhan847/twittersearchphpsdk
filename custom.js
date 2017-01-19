02e014c8cc1033edf078f9f3cf506f77898a1095
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-19 17:05:12 +0500

    changes

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     init();
 });
 
@@ -6,18 +6,19 @@ function init() {
     $.ajax({
         // Post select to url.
         type: 'get',
+        datatype: "json",
         url: 'http://phpstack-13267-59727-161633.cloudwaysapps.com/twitter/twittersearchphpsdk/index2.php', // expected returned data format.
         statusCode: {
-            200: function (response) {
-                $("#twitter-profiles").html(response);
-            },
-            422: function (response) {
-                alert('Error Getting Data');
-            },
-            400: function (response) {
-                alert('URL Not Found', 4000);
+                200: function (response) {
+                    $("#twitter-profiles").html(response);
+                },
+                422: function (response) {
+                   alert('Error Getting Data');
+                },
+                400: function (response) {
+                    alert('URL Not Found', 4000);
+                }
             }
-        }
     });
 
 }
