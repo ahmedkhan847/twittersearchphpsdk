9de06b1d7cef82bcb07c0bde487919f688296ca3
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-19 19:34:11 +0500

    finalyzing changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "vendor/autoload.php";
 use Twitter\Lists\Lists;
 
 $list = new Lists();
-$list->setToken("sVRnCXPUplLFePmzOqqoir8oR","PzmlGmm6UoJluWupC7js2hx025IwaY8gLD4I8BanQZtwS2g2Ql");
+$list->setToken("YOUR Consumer Key","YOUR Consumer Secret-Key");
 $value = ["list_id" => "821989883380199429",
 "count" => "100"];
 $lists = $list->getListMembers($value);
