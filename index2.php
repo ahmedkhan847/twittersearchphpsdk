dad627a81ecb091a5c205b6275dd398ad6ea3209
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-20 12:20:06 +0500

    changes has done

--- index2.php
+++ index2.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Origin: http://www.cloudways.com');
 include "vendor/autoload.php";
 
 use Twitter\Lists\Lists;
-
+$listid = $_GET['q'];
 $list = new Lists();
 $list->setToken("sVRnCXPUplLFePmzOqqoir8oR","PzmlGmm6UoJluWupC7js2hx025IwaY8gLD4I8BanQZtwS2g2Ql");
-$value = ["list_id" => "821989883380199429",
+$value = ["list_id" => $listid,
 "count" => "100"];
 $lists = $list->getListMembers($value);
 $profiles = array();
