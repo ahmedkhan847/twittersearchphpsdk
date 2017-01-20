e05c7af0c5c82e25a6ffd732752ecd53260cea68
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-20 12:21:40 +0500

    Final changing

--- custom.js
+++ custom.js
@@ -8,6 +8,7 @@ function init() {
         type: 'get',
         datatype: "json",
         url: 'http://phpstack-13267-59727-161633.cloudwaysapps.com/twitter/twittersearchphpsdk/index2.php', // expected returned data format.
+        data: { 'q' : query},
         statusCode: {
                 200: function (response) {
                     $("#twitter-profiles").html(JSON.parse(response));
