5538d8ecb16d0cc4da12adcf24f99a7599b330ab
Ahmed Khan <ahmedkhan_847@hotmail.com>
2017-01-20 12:16:50 +0500

    reindexing site

--- index.php
+++ index.php
@@ -5,10 +5,9 @@ include "vendor/autoload.php";
 use Twitter\Lists\Lists;
 
 $list = new Lists();
-$list->setToken("YOUR Consumer Key","YOUR Consumer Secret-Key");
-$value = ["list_id" => "821989883380199429",
-"count" => "100"];
-$lists = $list->getListMembers($value);
+$list->setToken("sVRnCXPUplLFePmzOqqoir8oR","PzmlGmm6UoJluWupC7js2hx025IwaY8gLD4I8BanQZtwS2g2Ql");
+$value = ["user_id" => "70902893"];
+$lists = $list->getUserList($value);
 echo "<pre>";
 print_r($lists);
 echo "</pre>";
